6e556d6062270ba027df48699ab5aca5747ee7f4
Ivan Baena <ivanbaena@Ivans-MBP.home>
2023-02-06 19:04:31 -0500

    feat: finish dev on button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from "react";
-import Button from './components/Button'
+import Button from "./components/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>Click me!!</Button>
+        <Button success rounded outline className="mb-1">
+          <GoBell />
+          Click me!!
+        </Button>
       </div>
       <div>
-        <Button danger outline>Buy Now!</Button>
+        <Button danger outline>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
       </div>
       <div>
-        <Button warning>See Deal!!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!!
+        </Button>
       </div>
       <div>
-        <Button secondary outline>Hide Ads!</Button>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
       </div>
       <div>
-        <Button secondary rounded>Something!</Button>
+        <Button primary rounded>
+          Something!
+        </Button>
       </div>
     </div>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cn from "classnames";
 
 function Button({
   children,
@@ -10,8 +11,24 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
-  return <button>{children}</button>;
+  const classes = cn(rest.className, "px-3 py-1.5 border flex items-center", {
+    "text-white border-blue-500 bg-blue-500": primary,
+    "text-white border-gray-900 bg-gray-900": secondary,
+    "text-white border-green-500 bg-green-500": success,
+    "text-white border-yellow-400 bg-yellow-400": warning,
+    "text-white border-red-500 bg-red-500": danger,
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-900": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-400": outline && warning,
+    "text-red-500": outline && danger,
+  });
+
+  return <button {...rest} className={classes} >{children}</button>;
 }
 
 Button.propTypes = {
